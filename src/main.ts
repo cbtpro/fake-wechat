35371fe25c66fae9897dea1f2208f3099ef66b2f
chenbitao <cbtpro@163.com>
2023-06-11 04:31:49 +0800

    feat: pinia增加持久化方案

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+/**
+ * pinia持久化方案 pinia-plugin-persistedstate
+ * https://prazdevs.github.io/pinia-plugin-persistedstate/guide/
+ */
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,22 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useCounterStore = defineStore(
+  'counter',
+  () => {
+    const count = ref(0)
+    const doubleCount = computed(() => count.value * 2)
+    const increment = () => {
+      count.value++
+    }
 
-  return { count, doubleCount, increment }
-})
+    const reduce = () => {
+      count.value--
+    }
+
+    return { count, doubleCount, increment, reduce }
+  },
+  {
+    persist: true
+  }
+)
