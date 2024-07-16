dd29692c569af45a22126bc9fc780e95fe319c8d
chenbitao <cbtpro@163.com>
2024-07-16 15:04:02 +0800

    feat: 通讯录代码

--- src/views/FriendListView.vue
+++ src/views/FriendListView.vue
@@ -14,7 +14,131 @@
  limitations under the License.
 -->
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const friendList = ref([
+  {
+    type: 'menu',
+    text: '新的朋友'
+  },
+  {
+    type: 'menu',
+    text: '仅聊天的朋友'
+  },
+  {
+    type: 'menu',
+    text: '群聊'
+  },
+  {
+    type: 'menu',
+    text: '标签'
+  },
+  {
+    type: 'menu',
+    text: '公众号'
+  },
+  {
+    type: 'menu',
+    text: '企业微信联系人'
+  },
+  {
+    type: 'Index',
+    text: '我的企业',
+    children: [
+      {
+        type: 'menu',
+        text: '深圳市税务局移动办税'
+      }
+    ]
+  },
+  {
+    type: 'IndexText',
+    text: 'A',
+    children: [
+      {
+        type: 'menu',
+        text: '阿娟'
+      },
+      {
+        type: 'menu',
+        text: '阿军'
+      },
+      {
+        type: 'menu',
+        text: 'AI'
+      }
+    ]
+  },
+  {
+    type: 'IndexText',
+    text: 'B',
+    children: [
+      {
+        type: 'menu',
+        text: '白浅'
+      },
+      {
+        type: 'menu',
+        text: '大B哥'
+      },
+      {
+        type: 'menu',
+        text: '大哥'
+      }
+    ]
+  },
+  {
+    type: 'IndexText',
+    text: 'C',
+    children: [
+      {
+        type: 'menu',
+        text: '陈小二'
+      },
+      {
+        type: 'menu',
+        text: '陈小三'
+      },
+      {
+        type: 'menu',
+        text: '陈小四'
+      }
+    ]
+  },
+  {
+    type: 'IndexText',
+    text: 'D',
+    children: [
+      {
+        type: 'menu',
+        text: '丁丁'
+      },
+      {
+        type: 'menu',
+        text: '丁丁'
+      },
+      {
+        type: 'menu',
+        text: '丁丁'
+      }
+    ]
+  },
+  {
+    type: 'IndexText',
+    text: 'Z',
+    children: [
+      {
+        type: 'menu',
+        text: '张霞'
+      }
+    ]
+  }
+])
+const indexList = computed(() => {
+  return friendList.value.filter((item) => item.type === 'IndexText').map((item) => item.text)
+})
+</script>
 
 <template>
   <van-nav-bar :fixed="true" :border="false" title="通讯录">
@@ -23,29 +147,14 @@
     </template>
   </van-nav-bar>
   <body-container>
-    <van-index-bar>
-      <van-cell title="新的朋友" />
-      <van-cell title="仅聊天的朋友" />
-      <van-cell title="群聊" />
-      <van-cell title="标签" />
-      <van-cell title="公众号" />
-      <van-cell title="企业微信联系人" />
-
-      <van-index-anchor index="我的企业" />
-      <van-cell title="深圳市税务局移动办税" />
-
-      <van-index-anchor index="A" />
-      <van-cell title="阿娟" />
-      <van-cell title="阿军" />
-      <van-cell title="AI" />
-
-      <van-index-anchor index="B" />
-      <van-cell title="白浅" />
-      <van-index-anchor index="D" />
-      <van-cell title="大B哥" />
-      <van-cell title="大哥" />
-      <van-index-anchor index="Z" />
-      <van-cell title="张霞" />
+    <van-index-bar :index-list="indexList">
+      <template v-for="(item) in friendList" :key="item.text">
+        <van-cell v-if="item.type === 'menu'" :title="item.text" />
+        <van-index-anchor v-else-if="item.type === 'IndexText'" :index="item.text" />
+        <template v-if="item.children">
+          <van-cell v-for="(child) in item.children" :title="child.text" :key="child.text" />
+        </template>
+      </template>
     </van-index-bar>
   </body-container>
   <body-footer />
